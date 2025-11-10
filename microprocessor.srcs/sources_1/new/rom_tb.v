c3d465abcfac345349b18bc8051fef1c96a1c67c
sabrina553 <general@sabrinalily.uk>
2025-11-10 03:07:49 +0000

    display values in console after each switch.

--- microprocessor.srcs/sources_1/new/rom_tb.v
+++ microprocessor.srcs/sources_1/new/rom_tb.v
@@ -20,20 +20,24 @@ module rom_tb;
 always #5  clk = ! clk;
     initial begin 
         clk = 0;
-        #10 romIn1 = 5'b01010; //10 -> 40159
-        #10 romIn2 = 5'b11010; //26 -> 45177
-        
-        $display("-------displaying romOut1-------");
+        //First Test
+        #10 romIn1 = 5'b01010; //10 -> 69723
+        #10 romIn2 = 5'b11010; //26 -> 91034        
+        #10 $display("-------displaying romOut-------");
         $display("\t %0d",romOut1);
         $display("\t %0d",romOut2);     
-          
-        #10 romIn1 = 5'b10000; //10 -> 40159
-        #10 romIn2 = 5'b01101; //26 -> 45177
         
-        #10 romIn1 = 5'b11111; //10 -> 40159
-        #10 romIn2 = 5'b01101; //26 -> 45177
+        //Second Test
+        #10 romIn1 = 5'b10000; //16 -> 22853
+        #10 romIn2 = 5'b01111; //15 -> 85087        
+        #10 $display("-------displaying romOut-------");
+        $display("\t %0d",romOut1);
+        $display("\t %0d",romOut2);
         
-        $display("-------displaying romOut1-------");
+        //Third Test
+        #10 romIn1 = 5'b11111; //31 -> 0
+        #10 romIn2 = 5'b01101; //13 -> 33726        
+        #10 $display("-------displaying romOut-------");
         $display("\t %0d",romOut1);
         $display("\t %0d",romOut2);
     end
