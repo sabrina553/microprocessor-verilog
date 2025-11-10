c7193b96e7bb30e78f130f00ebf2ef7dd7772777
sabrina553 <general@sabrinalily.uk>
2025-11-10 17:07:07 +0000

    Basic testbench. demonstrates working adder in alu, and uses monitor to update logs on change

--- microprocessor.srcs/sources_1/new/alu_tb.sv
+++ microprocessor.srcs/sources_1/new/alu_tb.sv
@@ -18,20 +18,36 @@ module alu_tb;
       .outputAlu(outputAlu)
   );
 
+  function static compare(int a, int b);
+    if (a == b) begin
+      return 1;
+    end
+  endfunction
+
   always #5 clk = !clk;
   initial begin
     clk = 0;
+    #10;
+    opcode = 6'b000100;  // Addition
+    inputA = 32'hFF;  // 255
+    inputB = 32'hFF;  //+255 = 510
+    #10;
+    inputA = 32'hbeef;  // 48879
+    inputB = 32'hcafe;  //+51966 = 100845
+    #10;
+    inputA = 32'hdead;  // 57005
+    inputB = 32'haa55;  //+43605 = 100610
+  end
 
-
-
-
-
+  //monitors for change in variables as below (suppose the `$` excludes time)
+  initial begin
+    $monitor("Time=%0t \t %d=iA %d=iB %d=op %d=out", $time, inputA, inputB, opcode, outputAlu);
   end
 
 
 endmodule
 
 
-// 	in1 <= X"0001ffff", X"0FAFffff" after 20 ns, X"F000ffff" after 40ns;
-// 	in2 <= X"0100ffff", X"7FFFffff" after 10ns, X"FFFFffff" after 30ns;
-// 	in3 <= "101000";
+//in1 <= X"0001ffff", X"0FAFffff" after 20 ns, X"F000ffff" after 40ns;
+//in2 <= X"0100ffff", X"7FFFffff" after 10ns, X"FFFFffff" after 30ns;
+//in3 <= "101000";
