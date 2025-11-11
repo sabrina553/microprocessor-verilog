f47b9bdfdf693a058d4b9c56b595d1efbc70d062
sabrina553 <general@sabrinalily.uk>
2025-11-11 17:13:32 +0000

    base for tb_top

--- microprocessor.srcs/sources_1/two/alu_tb_top.sv
+++ microprocessor.srcs/sources_1/two/alu_tb_top.sv
@@ -1,5 +1,5 @@
-// `include "alu_pkg.sv"
-`include "alu.sv"
+`include "alu_pkg.sv"
+`include "alu.v"
 `include "alu_if.sv"
 
 module alu_tb_top;
@@ -11,14 +11,25 @@ module alu_tb_top;
   //connect interface to the DUT
   alu dut (
       vif.sig_clock,
-      vif.sig_ina,
-	  vif.sig_inb,
+      vif.sig_inA,
+      vif.sig_inB,
+      vif.sig_opcode,
+      vif.sig_out
   );
 
-endmodule
-logic sig_clock;
-logic sig_inA;
-logic sig_inB;
-logic sig_opcode;
+  initial begin
+    //registers the interface in the configuration block so that other blocks can use it
+    uvm_resource_db#(virtual alu_if)::set(.scope("ifs"), .name("alu_if"), .val(vif));
 
-logic sig_out;
+    //execute the test
+    run_test();
+  end
+
+  //variable initialisation
+  initial begin
+    vif.sig_clock <= 1'b0;
+  end
+
+  always #5 vif.sig_clock = ~vif.sig_clock;
+
+endmodule
