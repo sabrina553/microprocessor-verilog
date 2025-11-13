33a5fce33e8b282c7b901469a48fd1da13a62103
sabrina553 <general@sabrinalily.uk>
2025-11-13 15:25:07 +0000

    base uvm complete* untested

---
+++ microprocessor.srcs/sources_1/two/alu_agent.sv
@@ -0,0 +1,37 @@
+class alu_agent extends uvm_agent;
+	`uvm_component_utils(alu_agent)
+
+	//analysis ports to connect the monitors to scoreboard
+	uvm_analysis_port#(alu_transaction) agent_ap_before;
+	uvm_analysis_port#(alu_transaction) agent_ap_after;
+
+	alu_sequencer sa_seqr;
+	alu_driver sa_drvr;
+	alu_monitor_before sa_mon_before;
+	alu_monitor_after sa_mon_after;
+
+	function new(string name, uvm_component parent);
+		super.new(name, parent);
+	endfunction : new
+
+	function void build_phase(uvm_phase phase);
+		super.build_phase(phase);
+
+		agent_ap_before = new(.name("agent_ap_before"), .parent(this));
+		agent_ap_after = new(.name("agent_ap_after"), .parent(this));
+
+		sa_seqr = alu_sequencer::type_id::create(.name("sa_seqr"), .parent(this));
+		sa_drvr = alu_driver::type_id::create(.name("sa_drvr"), .parent(this));
+		sa_mon_before = alu_monitor_before::type_id::create(.name("sa_mon_before"), .parent(this));
+		sa_mon_after = alu_monitor_after::type_id::create(.name("sa_mon_after"), .parent(this));
+	endfunction : build_phase
+
+	function void connect_phase(uvm_phase_phase);
+		super.connect_phase(phase);
+		sa_drvr.seq_item_port.connect(sa_seqr.seq_item_export);
+		sa_mon_before.mon_ap_before.connect(agent_ap_before);
+		sa_mon_after.mon_ap_after.connect(agent_ap_after);
+	endfunction : connect_phase
+endclass : alu_agent
+
+
--- microprocessor.srcs/sources_1/two/alu_pkg.sv
+++ microprocessor.srcs/sources_1/two/alu_pkg.sv
@@ -1,11 +1,12 @@
 package alu_pkg;
   import uvm_pkg::*;
+
   `include "alu_sequencer.sv"
-  //`include "alu_monitor.sv"
+  `include "alu_monitor.sv"
   `include "alu_driver.sv"
-  //`include "alu_agent.sv"
-  //`include "alu_scoreboard.sv"
+  `include "alu_agent.sv"
+  `include "alu_scoreboard.sv"
   //`include "alu_config.sv"
-  //`include "alu_env.sv"
-  //`include "alu_test.sv"
+  `include "alu_env.sv"
+  `include "alu_test.sv"
 endpackage : alu_pkg
